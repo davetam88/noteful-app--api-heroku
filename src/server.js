89eed85cabb6e3f880a89c2a0b29b9de05b5943e
David Tam <davetam88@gmail.com>
2022-12-11 20:30:35 -0500

    change server messaage, and before heroku push after links it back to this repo

--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
-const knex = require('knex')
-const app = require('./app')
-const { PORT, DATABASE_URL } = require('./config')
+const knex = require("knex");
+const app = require("./app");
+const { PORT, DATABASE_URL } = require("./config");
 
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: DATABASE_URL,
-})
-
-// Using app.set('property-name', 'property-value') we 
-// can set a property called 'db' and set the Knex instance as the value.
-app.set('db', db)
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
 });
 
+// Using app.set('property-name', 'property-value') we
+// can set a property called 'db' and set the Knex instance as the value.
+app.set("db", db);
+
+app.listen(PORT, () => {
+  console.log(`Noteful-app-Server listening at http://localhost:${PORT}`);
+});
